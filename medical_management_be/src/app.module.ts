3eea5e67b61052ad2c074155bf90abbc08ec83ba
ThanhTai-IT <vovan.thanhtai7777@gmail.com>
2025-11-19 20:53:52 +0700

    update: thêm JSDoc comment cho AppModule

--- medical_management_be/src/app.module.ts
+++ medical_management_be/src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { FeatureModuleModule } from '@/modules/feature-module.module';
 
+/**
+ * Module chính của ứng dụng NestJS
+ * Quản lý tất cả các module và dependencies
+ */
 @Module({
   imports: [CoreModule, FeatureModuleModule],
   controllers: [AppController],
