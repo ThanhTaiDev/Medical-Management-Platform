71e9ecbbde45e2c41648117ceb76f47e58a58ec5
Thanh Tai <vovan.thanhtai7777@gmail.com>
2025-12-02 18:32:20 +0700

    Update: them class documentation cho AppModule

--- medical_management_be/src/app.module.ts
+++ medical_management_be/src/app.module.ts
@@ -8,6 +8,12 @@ import { FeatureModuleModule } from '@/modules/feature-module.module';
  * Module chính của ứng dụng NestJS
  * Quản lý tất cả các module và dependencies
  */
+/**
+ * Root module của ứng dụng
+ * Import tất cả các modules và cấu hình global
+ * 
+ * @class AppModule
+ */
 @Module({
   imports: [CoreModule, FeatureModuleModule],
   controllers: [AppController],
