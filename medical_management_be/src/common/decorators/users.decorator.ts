ae258ae0ef35d4eead172cd41ade2415fe1ae424
Thanh Tai <vovan.thanhtai7777@gmail.com>
2025-12-02 18:31:32 +0700

    Update: them JSDoc cho UserInfo decorator

--- medical_management_be/src/common/decorators/users.decorator.ts
+++ medical_management_be/src/common/decorators/users.decorator.ts
@@ -1,4 +1,10 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+/**
+ * Decorator để lấy thông tin user từ request
+ * Sử dụng trong controllers để inject user info
+ * 
+ * @returns {ParameterDecorator} Parameter decorator
+ */
 export const UserInfo = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
